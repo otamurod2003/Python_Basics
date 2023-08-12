5825cb29b992a7c579f580209967c73a4129b391
otamurod2003 <otamurodpirnapasov2003@gmail.com>
2023-08-12 09:05:15 +0500

    Birinchi dars. Python dasturlash tiliga kirish. Print funksiyasi

---
+++ 01_Print_function.py
@@ -0,0 +1,33 @@
+#Muallif: Otamurod Pirnapasov
+#Bu kodlar 2023-yil 12-avgustda yozildi.
+#bir tirnoq bilan matnni ekranga chiqarish
+print('Hello world') #Hello world
+
+#qo'sh tirnoq bilan matnni ekranga chiqarish
+print("Hello Olma")
+
+#Ekranga chiqarilayotgan matn ichida qo'shtirnoq kelgan payt (1)
+print('Assalomu aleykum, mening ismim O\'tkirbek')
+
+#2-holat
+print("Mening ismim O'tkirbek")
+
+#3-holat => bir necha qatorda apostrof chiqarish
+print('''Assalomu aleykum,
+O'zbekiston o'lkasi. Meni kut va men qaytarman''')
+
+#Arifmetik amallar
+# "+"
+print(12+5)#17
+print(15-5)#10
+print(13*6)#78
+print(15/5)#3
+print(16//5)# 3=> Bo'linmaning butun qismini oladi
+print(2**5)#32=> 2 sonini 5-darajaga ko'taradi
+
+
+#matnlar bilan arifmentik amallarni bitta funksiya ichida yozish
+print("8*8= ", 8*8, " bo'ladi")
+print("48 ni  10 ga bo'lganda hosil bo'lgan sonning butun qismi",48//10, " ga teng bo'ladi")
+
+
