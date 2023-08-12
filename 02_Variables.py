7aa3e8111d04d5af5ba6d5e208823cbeb3d70937
otamurod2003 <otamurodpirnapasov2003@gmail.com>
2023-08-12 09:22:21 +0500

    Added variables

---
+++ 02_Variables.py
@@ -0,0 +1,15 @@
+#o'zgaruvchilar bilan ishlaymiz
+#Muallif: Otamurod Pirnapasov
+#Dastur yaratilgan sana: 12.08.2023
+
+#O'zgaruvchi,  o'z nomi bilan o'zgaruvchi. 
+#Yaniki unga berilgan qiymatlar hohishingizga qarab o'zgaradi
+#O'zgaruvchilarga chiroyli nom berish kerak hamda uni e'lon qilishda 
+#O'zgaruvchi nomida lotin alifbosi harflari, raqamlar, pastki chiziq ham bo'lishi mumkin
+#O'zgaruvchi nomida katta va kichik harflar turlicha talqin qilinadi
+#O'zgaruvchi nomi bir nechta so'zdan iborat bo'lsa uni bo'sh joy bilan ajratib bo'lmaydi
+#o'zgaruvchi nomi kalit so'z bo'la olmaydi. Masalan for, if, else, elif, while, in, of va hokazo
+ism = "Otamurod Pirnapasov"  #String turidagi o'zgaruvchi (str)
+yosh = 20 # sonli malumotlar (int)
+
+print("Foydalanuchi ismi:", ism,".","Yoshi:", yosh)
