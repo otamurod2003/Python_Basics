9293c75837b21a7ea39e28a9de2a2c8a0a7225ea
otamurod2003 <otamurodpirnapasov2003@gmail.com>
2023-08-20 22:11:22 +0500

    added lists ex and modified numbers ex

--- 04_numbers.py
+++ 04_numbers.py
@@ -2,3 +2,8 @@
 #Muallif: Otamurod Pirnapasov
 #Dastur yaratilgan sana: 12.08.2023
 
+a= 20;
+b=5.5;
+print(type(a)) #integer turidagi malumot
+print(type(b)) #float turidagi malumot
+
---
+++ 05_lists.py
@@ -0,0 +1,16 @@
+x= int(input("x sonni kiriting:"))
+y=int(input("y sonni kiriting:"))
+z=int(input("z sonni kiriting:"))
+if((x>y) and (x>z)):
+    print("x katta")
+elif((y>x) and (y>z)): 
+    print("y katta")
+else:
+    print("z katta")
+
+
+soni = (input('Nechta son kiritmoqchisiz'))
+
+for k in soni :
+    print(input(k, ' sonni kiriting: '))
+                 
