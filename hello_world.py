ed46ded3a67fe9532dfa8964ffab3a1e0f0c0569
otamurod2003 <otamurodpirnapasov2003@gmail.com>
2023-10-31 20:41:36 +0500

    added hello world.

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+a = int(input("A songa qiymat bering: "))
+b = int(input("B songa qiymat bering: "))
+c = a+b
+print("Natija= ", c)
+if c>0 :
+    print("Natija musbat")
+elif c==0 :
+    print("Natija 0")
+elif c<0 :
+    print("Natija manfiy")
+
+
